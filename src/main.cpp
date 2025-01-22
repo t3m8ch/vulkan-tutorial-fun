2f52ee040c146f4e8e7b5a435008f236332bb308
Kudyakov Artem <t3m8ch@yandex.ru>
2025-01-22 23:46:48 +0400

    Drawing triangle -> Setup -> Instance

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
+#include <vulkan/vulkan_core.h>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
 #include <cstdlib>
 #include <iostream>
+#include <stdexcept>
+#include <vector>
 
 constexpr uint32_t WIDTH = 800;
 constexpr uint32_t HEIGHT = 600;
 constexpr std::string_view TITLE = "Vulkan tutoial";
+constexpr std::string_view ENGINE = "No engine";
 
 class App {
 public:
@@ -19,6 +23,7 @@ public:
 
 private:
   GLFWwindow *window;
+  VkInstance instance;
 
   void initWindow() {
     glfwInit();
@@ -29,7 +34,7 @@ private:
     window = glfwCreateWindow(WIDTH, HEIGHT, TITLE.data(), nullptr, nullptr);
   }
 
-  void initVulkan() {}
+  void initVulkan() { createInstance(); }
 
   void mainLoop() {
     while (!glfwWindowShouldClose(window)) {
@@ -38,8 +43,77 @@ private:
   }
 
   void cleanup() {
+    vkDestroyInstance(instance, nullptr);
     glfwDestroyWindow(window);
     glfwTerminate();
+  }
+
+  void createInstance() {
+    VkApplicationInfo appInfo = getAppInfo();
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    auto avaiableExtensions = getAvaiableExtensions();
+    printExtensions(avaiableExtensions);
+
+    auto requiredExtensions = getRequiredExtensions();
+
+    createInfo.enabledExtensionCount =
+        static_cast<uint32_t>(requiredExtensions.size());
+    createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+    createInfo.enabledLayerCount = 0;
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+      throw std::runtime_error("Failed to create instance");
+    }
+  }
+
+  VkApplicationInfo getAppInfo() {
+    VkApplicationInfo appInfo{};
+
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = TITLE.data();
+    appInfo.applicationVersion = VK_MAKE_VERSION(0, 0, 1);
+    appInfo.pEngineName = ENGINE.data();
+    appInfo.engineVersion = VK_MAKE_VERSION(0, 0, 1);
+    appInfo.apiVersion = VK_API_VERSION_1_3;
+
+    return appInfo;
+  }
+
+  std::vector<VkExtensionProperties> getAvaiableExtensions() {
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount,
+                                           extensions.data());
+
+    return extensions;
+  }
+
+  void printExtensions(const std::vector<VkExtensionProperties> extensions) {
+    std::cout << extensions.size() << " extensions:" << std::endl;
+    for (const auto &extension : extensions) {
+      std::cout << '\t' << extension.extensionName << std::endl;
+    }
+  }
+
+  std::vector<const char *> getRequiredExtensions() {
+    uint32_t glfwExtensionCount = 0;
+    const char **glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    std::vector<const char *> requiredExtensions;
+    for (uint32_t i = 0; i < glfwExtensionCount; i++) {
+      requiredExtensions.emplace_back(glfwExtensions[i]);
+    }
+
+    requiredExtensions.emplace_back(
+        VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+
+    return requiredExtensions;
   }
 };
 
